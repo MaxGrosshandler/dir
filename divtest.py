af5dae79201fdfbf7a1cdc18d7eda20226a73264
Max Grosshandler <maxgrosshandler@gmail.com>
2015-08-03 08:05:35 -0500

    Updated divtest to work with requests to check http status code

--- divtest.py
+++ divtest.py
@@ -1,11 +1,13 @@
-def divide(a,b):
-    """
-    >>> divide(4,2)
-    2
-    >>> divide(90,9)
-    10
-    """
-    return (a / b)
+import requests
+
+
+try:
+    r = requests.head("http://stackoverflow.com")
+    return(r.status_code)
+except requests.ConnectionError:
+    print("failed to connect")
+
+    
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
