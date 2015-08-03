06dc2ce4ee0306167516139699a926c1eac5bb24
Max Grosshandler <maxgrosshandler@gmail.com>
2015-08-03 07:59:57 -0500

    New doctest test

---
+++ divtest.py
@@ -0,0 +1,11 @@
+def divide(a,b):
+    """
+    >>> divide(4,2)
+    12
+    >>> divide(90,9)
+    10
+    """
+    return (a / b)
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
