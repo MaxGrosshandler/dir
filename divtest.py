8298f1a60b561170e1bd023107fa0b90e6ff9f03
Max Grosshandler <maxgrosshandler@gmail.com>
2015-08-03 08:25:01 -0500

    Switched requests with httplib on divtest

--- divtest.py
+++ divtest.py
@@ -1,14 +1,15 @@
-import requests
+import httplib
 
-def reqtest(a):
-    """
-    >>> reqtest("questions")
-    200
-    """
-    r = requests.head("http://stackoverflow.com" + "\"" + a)
-    return(r.status_code)
 
-    
+def get_status_code(host, path=("/" + a)):
+    """
+    print get_status_code("stackoverflow.com", "questions")
+    """
+    conn = httplib.HTTPConnection(host)
+    conn.request("HEAD", path)
+    return conn.getresponse().status
+
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
